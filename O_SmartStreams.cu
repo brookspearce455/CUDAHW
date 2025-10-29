7c8fe778231a424d8513d53cc02b170b6ff46a05
brookspearce455 <22604bap@gmail.com>
2025-10-29 10:41:32 -0500

    Update O_SmartStreams.cu

--- O_SmartStreams.cu
+++ O_SmartStreams.cu
@@ -81,9 +81,9 @@ void setUpCudaDevices()
 		exit(0);
 	}
 	
-	cudaStreamCreate(&Stream0);
+	cudaStreamCreateWithFlags(&Stream0,cudaStreamNonBlocking);
 	cudaErrorCheck(__FILE__, __LINE__);
-	cudaStreamCreate(&Stream1);
+	cudaStreamCreateWithFlags(&Stream1,cudaStreamNonBlocking);
 	cudaErrorCheck(__FILE__, __LINE__);
 	
 	BlockSize.x = BLOCK_SIZE;
@@ -196,28 +196,29 @@ int main()
 	
 	for(int i = 0; i < ENTIRE_DATA_SET; i += DATA_CHUNKS*2)
 	{
-		cudaMemcpyAsync(A0_GPU,A_CPU+i/2, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream0);
+		cudaMemcpyAsync(A0_GPU,A_CPU+i, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream0);
 		cudaErrorCheck(__FILE__, __LINE__);
-		cudaMemcpyAsync(B0_GPU,B_CPU+i/2, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream0);
+		cudaMemcpyAsync(B0_GPU,B_CPU+i, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream0);
 		cudaErrorCheck(__FILE__, __LINE__);
 		
 		trigAdditionGPU<<<GridSize,BlockSize,0,Stream0>>>(A0_GPU,B0_GPU,C0_GPU,DATA_CHUNKS);
-		
-		cudaMemcpyAsync(C_CPU+i/2,C0_GPU, DATA_CHUNKS*sizeof(float),cudaMemcpyDeviceToHost,Stream0);
+		cudaMemcpyAsync(C_CPU+i,C0_GPU, DATA_CHUNKS*sizeof(float),cudaMemcpyDeviceToHost,Stream0);
 		cudaErrorCheck(__FILE__, __LINE__);
 		
+		if(i<=ENTIRE_DATA_SET-DATA_CHUNKS){
 		cudaMemcpyAsync(A1_GPU,A_CPU+DATA_CHUNKS+i, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream1);
 		cudaErrorCheck(__FILE__, __LINE__);
 		cudaMemcpyAsync(B1_GPU,B_CPU+DATA_CHUNKS+i, DATA_CHUNKS*sizeof(float),cudaMemcpyHostToDevice,Stream1);
 		cudaErrorCheck(__FILE__, __LINE__);
 		
 		trigAdditionGPU<<<GridSize,BlockSize,0,Stream1>>>(A1_GPU,B1_GPU,C1_GPU,DATA_CHUNKS);
-		
 		cudaMemcpyAsync(C_CPU+DATA_CHUNKS+i,C1_GPU, DATA_CHUNKS*sizeof(float),cudaMemcpyDeviceToHost,Stream1);
 		cudaErrorCheck(__FILE__, __LINE__);
+		}
+		
 	}
 	
-	// Make the CPU wait until the Streams have finishd before it continues.
+	// Make the CPU wait until the Streams have finished before it continues.
 	cudaStreamSynchronize(Stream0);
 	cudaStreamSynchronize(Stream1);
 	
